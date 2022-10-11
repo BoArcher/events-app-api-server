14f0ea9a27c122b64faf58d0742fada44d75ca65
BoArcher <itslukeh23@gmail.com>
2022-10-11 16:25:22 +0000

    Name change

--- src/app.config.js
+++ src/app.config.js
@@ -4,7 +4,7 @@
  * DO NOT store state here - must be part of a stateless service
  */
 class AppConfig {
-    team = 'Luke Hampson';
+    team = 'Luke William Edward Hampson';
     version = '1.0.1';
 }
 module.exports = AppConfig;
